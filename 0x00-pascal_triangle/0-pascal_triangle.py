2f149bc91478a66cfcc03034b61cb4e24b883c6c
ojijoebrain <ojijosephoji@gmail.com>
2024-08-08 10:33:00 +0000

    updated

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module that will return pascal triangle'''
+
+
+def pascal_triangle(n):
+    '''
+    lists of integers representing the Pascal’s triangle
+    '''
+    lists = []
+    if n == 0:
+        return lists
+    for k in range(n):
+        lists.append([])
+        lists[k].append(1)
+        if (k > 0):
+            for j in range(1, k):
+                lists[k].append(lists[k - 1][j - 1] + lists[k - 1][j])
+            lists[k].append(1)
+    return lists
