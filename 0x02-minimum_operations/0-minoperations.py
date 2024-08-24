5f281263c12ba62dc27597611aa0dfed47f5d4fc
ojijoebrain <ojijoebrain73@gmail.com>
2024-08-24 02:38:17 +0000

    update_again

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
 
 """
-This script contains a function that calculates the fewest number of operations
-needed to reach exactly `n` characters using Copy All and Paste operations.
+    Method that determines the number of minmum operations given n characters
 """
+
 
 def minOperations(n):
     """
-    Calculate the minimum number of operations needed to obtain exactly `n`
-    characters in the file.
-
-    Parameters:
-    n (int): The target number of characters.
-
-    Returns:
-    int: The minimum number of operations, or 0 if `n` is less than or equal to 1.
+        A function that calculates the fewest number of operations
+        needed to give a result of exactly n H characters in a file
+        args: n: Number of characters to be displayed
+        return:
+               number of min operations
     """
-    if n <= 1:
-        return 0
 
     now = 1
     start = 0
     counter = 0
-
     while now < n:
         remainder = n - now
-        if remainder % now == 0:
+        if (remainder % now == 0):
             start = now
             now += start
             counter += 2
         else:
             now += start
             counter += 1
-
     return counter
