4556d645061d4a5db6414ed96fb0f3410edd2956
ojijoebrain <ojijoebrain73@gmail.com>
2024-09-21 13:47:43 +0000

    starwars_characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,25 +3,31 @@
 const request = require('request');
 
 const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-request(url, (err, res, body) => {
+request({ url: url, strictSSL: false }, function (err, res, body) {
   if (err) {
-    return console.error(err);
-  }
-
-  if (res.statusCode !== 200) {
-    return console.error(`Error: ${res.statusCode}`);
+    console.error(err);
+    return;
   }
 
   const charactersArray = JSON.parse(body).characters;
-  charactersArray.forEach((character) => {
-    request(character, (err, res, body) => {
+
+  function printCharacterName(index) {
+    if (index >= charactersArray.length) {
+      return;
+    }
+
+    request({ url: charactersArray[index], strictSSL: false }, function (err, res, body) {
       if (err) {
-        return console.error(err);
+        console.error(err);
+        return;
       }
+
       console.log(JSON.parse(body).name);
+      printCharacterName(index + 1);  // Recursively call the next character
     });
-  });
+  }
+
+  printCharacterName(0);  // Start with the first character
 });
