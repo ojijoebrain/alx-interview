f989c67bcec8fc4e7a965d6f0b540cde3d508851
ojijoebrain <ojijoebrain73@gmail.com>
2024-10-15 10:29:39 +0000

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 """
-Prime Game
+Module: Game of choosing Prime numbers
 """
 
-def is_prime(n):
-    """Check if a number is prime."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+
+def primeNumbers(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
+
 
 def isWinner(x, nums):
-    """Determines the winner after x rounds."""
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        prime_set = [i for i in range(1, n + 1) if is_prime(i)]
-        maria_turn = True
-
-        while prime_set:
-            prime = prime_set[0]
-            prime_set = [i for i in prime_set if i % prime != 0]
-
-            if maria_turn:
-                maria_turn = False
-            else:
-                maria_turn = True
-
-        if not maria_turn:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+    Maria = Ben = 0
+    for i in range(x):
+        primeNos = primeNumbers(nums[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
